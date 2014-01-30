08119b09462c65e254d6a94efd144c29a063a092
Eric Merritt <ericbmerritt@gmail.com>
2014-01-30 12:36:14 -0800

    refactor dynamodb.{ml,mli} to get it to a good base
    There was no mli file and the types where not well specified. This
    gets it to a known good base point.

--- dynamodb.ml
+++ dynamodb.ml
@@ -2,21 +2,21 @@ open Core
 open Core.Std
 open Async.Std
 
+type t = {auth: Auth.t;
+          region: String.t;
+          url: Uri.t}
+
 type return_consumed_capacity = NONE | TOTAL
 
 type query =
-  | Binary of string
-  | BinarySet of string list
-  | Int of int
-  | IntSet of int list
-  | Float of float
-  | FloatSet of float list
-  | String of string
-  | StringSet of string list
-
-type t = {auth: Auth.t;
-          region: string;
-          url: Uri.t}
+  | Binary of String.t
+  | BinarySet of String.t List.t
+  | Int of Int.t
+  | IntSet of Int.t List.t
+  | Float of Float.t
+  | FloatSet of Float.t List.t
+  | String of String.t
+  | StringSet of String.t List.t
 
 let service = "dynamodb"
 let api_version = "DynamoDB_20120810"
@@ -80,7 +80,7 @@ let get_item sys ?(attributes_to_get = [])
                                 ("TableName", `String table_name)]])) in
   Request.post sys.auth service [("x-amz-target", request_spec)] json sys.url
 
-let create access_id secret_key region ?(url = None) =
+let create ?url access_id secret_key region =
   let make_url = match url with
     | None -> Uri.of_string ("http://dynamodb." ^
                                region ^ ".amazonaws.com")
