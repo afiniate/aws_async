65e8cc3d9f4e88992de67e10eb2222e02f084246
Eric Merritt <ericbmerritt@gmail.com>
2014-01-30 12:36:54 -0800

    split aws into aws_auth and dynamolibs

--- auth_headers.mli
+++
@@ -1,9 +0,0 @@
-(**
- * Process a set of headers in the manner indicated by aws for all other
- * requests
- *)
-type header = (string * string)
-type t = header list
-
-val process: Uri.t -> t -> (Core.Std.Unix.tm * t * t)
-val print: t -> unit
--- auth_s.mli
+++
@@ -1,5 +0,0 @@
-module S : sig
-    type t = {secret_access_key: string;
-              access_key_id: string;
-              default_region: string}
-end
