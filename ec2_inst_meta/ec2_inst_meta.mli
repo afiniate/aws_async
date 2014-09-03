a111773d6c955a2ee6ba5f5da8d34b449399b973
Samuel Rivas <samuel.rivas@klarna.com>
2014-09-03 11:51:50 +0200

    change unit by Unit.t

--- ec2_inst_meta/ec2_inst_meta.mli
+++ ec2_inst_meta/ec2_inst_meta.mli
@@ -5,14 +5,14 @@ open Async.Std
  * This module provides access to the instance metadata of an ec2 instance.
  *)
 
-val get_availability_zone: unit -> (String.t, Exn.t) Deferred.Result.t
+val get_availability_zone: Unit.t -> (String.t, Exn.t) Deferred.Result.t
 (** Get the availability zone from the instance metadata *)
 
-val get_region: unit -> (String.t, Exn.t) Deferred.Result.t
+val get_region: Unit.t -> (String.t, Exn.t) Deferred.Result.t
 (** Get the region from the instance metadata *)
 
 val get_role: string -> (Ec2im_iam_role_t.desc, Exn.t) Deferred.Result.t
 (** Get the region from the instance metadata *)
 
-val get_user_data: unit -> (String.t, Exn.t) Deferred.Result.t
+val get_user_data: Unit.t -> (String.t, Exn.t) Deferred.Result.t
 (** Returns the user data for the instance (if there is any) *)
