586ff9cf8e2e56ca7e3d1fd87386893472683aad
Samuel Rivas <samuel.rivas@klarna.com>
2014-09-03 10:38:58 +0200

    Functorise ec2_inst_meta
    This provides the same API as the previous versions (but with slightly different
    error reporting) plus a functor to create new modules providing a "fetcher". The
    default fetcher reads information from EC2's HTTP Api, but other fetchers can be
    provided in order to enable unit testing

--- ec2_inst_meta/ec2_inst_meta.ml
+++ ec2_inst_meta/ec2_inst_meta.ml
@@ -1,49 +1,63 @@
 open Core.Std
 open Async.Std
 
-type error_code =
-  | Unavailable_region
-  | Unavailable_role
-  | Unavailable_user_data with sexp
+type role_desc = Ec2im_iam_role_t.desc
 
-exception Err of error_code * String.t  with sexp
+module type Api = sig
+  val get_availability_zone : Unit.t -> (String.t, Exn.t) Deferred.Result.t
+  val get_region : Unit.t -> (String.t, Exn.t) Deferred.Result.t
+  val get_role : String.t -> (role_desc, Exn.t) Deferred.Result.t
+  val get_user_data : Unit.t -> (String.t, Exn.t) Deferred.Result.t
+end
 
-let make_err error_code status_code =
-  Err (error_code,
-       (Sexp.to_string (Cohttp.Code.sexp_of_status_code status_code)))
+module type Fetcher = sig
+  val fetch: String.t -> (String.t, Exn.t) Deferred.Result.t
+end
 
-let process_result error_code (resp, body) =
-  let status_code = Cohttp.Response.status resp in
-  if `OK = status_code then
-    Cohttp_async.Body.to_string body >>| fun s -> Ok s
-  else
-    return @@ Error (make_err error_code status_code)
+module Make (Fetcher : Fetcher) : Api = struct
+  let get_availability_zone () =
+    Fetcher.fetch "/meta-data/placement/availability-zone"
 
-let fetch_metadata error_cb path =
-  let base_url = "http://169.254.169.254/latest" in
-  let uri = Uri.of_string (base_url ^ path) in
-  Cohttp_async.Client.get uri
-  >>= process_result error_cb
+  let get_region () =
+    (* The zone is the region name + a one character zone idenifier. We
+       mainly just split off that zoneidentifier. This will cause problems
+       if there are ever more than 26 zones and AWS starts using more
+       letters. However, when that happens we can switch to regexp. *)
+    let parse_region_from_zone zone =
+      String.slice zone 0 @@ (String.length zone) - 1 in
+    get_availability_zone ()
+    >>|? parse_region_from_zone
 
-(* -------------------------------------------------------------------- *)
-(* Public API                                                           *)
-(* -------------------------------------------------------------------- *)
-let get_availability_zone () =
-  let path = "/meta-data/placement/availability-zone" in
-  fetch_metadata Unavailable_region path
+  let get_role role_name =
+    let path = "/meta-data/iam/security-credentials/" ^ role_name in
+    Fetcher.fetch path
+    >>|? Ec2im_iam_role_j.desc_of_string
 
-let get_region () =
-  (* The zone is the region name + a one character zone idenifier. We
-     mainly just split off that zoneidentifier. This will cause problems
-     if there are ever more than 26 zones and AWS starts using more
-     letters. However, when that happens we can switch to regexp. *)
-  let parse_region_from_zone zone =
-    String.slice zone 0 @@ (String.length zone) - 1 in
-  get_availability_zone () >>|? parse_region_from_zone
+  let get_user_data () =
+    Fetcher.fetch "/user-data"
+end
 
-let get_role role_name =
-  let path = "/meta-data/iam/security-credentials/" ^ role_name in
-  fetch_metadata Unavailable_role path >>|? Ec2im_iam_role_j.desc_of_string
+(* Production Fetcher *)
 
-let get_user_data () =
-  fetch_metadata Unavailable_user_data "/user-data"
+module Prod_fetcher : Fetcher = struct
+  (* The payload is the HTTP error code and the Uri that we tried to fetch *)
+  exception Cannot_fetch_metadata of Cohttp.Code.status_code * Uri.t with sexp
+
+  let process_result uri (resp, body) =
+    let status_code = Cohttp.Response.status resp in
+    if `OK = status_code then
+      Cohttp_async.Body.to_string body
+      >>| fun s -> Ok s
+    else
+      return @@ Error (Cannot_fetch_metadata (status_code, uri))
+
+  let fetch path =
+    let base_url = "http://169.254.169.254/latest" in
+    let uri = Uri.of_string (base_url ^ path) in
+    Cohttp_async.Client.get uri
+    >>= process_result uri
+end
+
+(* Production API *)
+
+include Make(Prod_fetcher)
--- ec2_inst_meta/ec2_inst_meta.mli
+++ ec2_inst_meta/ec2_inst_meta.mli
@@ -1,18 +1,37 @@
 open Core.Std
 open Async.Std
 
-(**
- * This module provides access to the instance metadata of an ec2 instance.
-*)
+(** Provides access to instance metadata when run in an ec2 instance *)
 
-val get_availability_zone: Unit.t -> (String.t, Exn.t) Deferred.Result.t
-(** Get the availability zone from the instance metadata *)
+module type Fetcher = sig
+  val fetch: String.t -> (String.t, Exn.t) Deferred.Result.t
+  (** Fetch the metadata referred by the argument
 
-val get_region: Unit.t -> (String.t, Exn.t) Deferred.Result.t
-(** Get the region from the instance metadata *)
+      The argument is a path to the required metadata, excluding the root URI
+      for the latest version. For example, the path "/iam/info". will be
+      translated into a GET to "http://169.254.169.254/latest/iam/info" by
+      the production Fetcher *)
+end
 
-val get_role: string -> (Ec2im_iam_role_t.desc, Exn.t) Deferred.Result.t
-(** Get the region from the instance metadata *)
+module type Api = sig
+  val get_availability_zone : Unit.t -> (String.t, Exn.t) Deferred.Result.t
+  (** Return the availability zone of the running instance *)
 
-val get_user_data: Unit.t -> (String.t, Exn.t) Deferred.Result.t
-(** Returns the user data for the instance (if there is any) *)
+  val get_region : Unit.t -> (String.t, Exn.t) Deferred.Result.t
+  (** Return the region of the running instance *)
+
+  val get_role : String.t -> (Ec2im_iam_role_t.desc, Exn.t) Deferred.Result.t
+  (** Return the role description of the running instance *)
+
+  val get_user_data : Unit.t -> (String.t, Exn.t) Deferred.Result.t
+  (** Return the user data of the running instance, if there are any *)
+end
+
+(** This functor delegates all side effects to the Fetcher so that we can unit
+    test this module in isolation. It has the same API as the main module, but
+    you must provide an alternative fetcher that simulates the HTTP interface
+    available in EC2 instances *)
+module Make (Fetcher : Fetcher) : Api
+
+(* Default Api using the production fetcher *)
+include Api
